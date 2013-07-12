e42da1b31ae1e2004a11d03dc8e0c18038c6e569
Matt <code@zonbi.org>
2013-07-13 01:35:22 +0200

    problem with the ethernet type

--- Arp/arp_who-has.py
+++ Arp/arp_who-has.py
@@ -5,10 +5,10 @@ from scapy.all import *
 
 print "building packet"
 a = ARP()
-a.hwtype="0x1"
-a.ptype="0x800"
-a.hwlen="6"
-a.plen="4"
+a.hwtype=int("0x1", 16)
+a.ptype=int("0x800", 16)
+a.hwlen=int("6")
+a.plen=int("4")
 a.op="who-has"
 a.pdst="172.16.0.1"
 a.hwsrc="00:0c:29:be:e4:77"
