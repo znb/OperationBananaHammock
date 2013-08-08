e99a030c27a6fc386937c3fd0a6842b77d729927
Matt <code@zonbi.org>
2013-08-08 21:07:06 +0200

    small changes

--- Arp/arp_who-has.py
+++ Arp/arp_who-has.py
@@ -4,11 +4,11 @@
 from scapy.all import *
 import sys
 
-sys.stdout.write("building packet: ")
-sys.stdout.write("Ethernet/")
+print "building packet: ",
+print "Ethernet/",
 e = Ether()
 e.type=int("0x806", 16)
-sys.stdout.write("Arp")
+print "Arp",
 a = ARP()
 a.hwtype=int("0x1", 16)
 a.hwlen=int("6")
@@ -16,10 +16,10 @@ a.plen=int("4")
 a.op="who-has"
 a.pdst="172.16.0.1"
 a.hwsrc="00:0c:29:be:e4:77"
-a.psrc="172.16.0.10"
+a.psrc="172.16.0.200"
 a.hwdst="ff:ff:ff:ff:ff:ff" 
 packet = e/a
 print "\n"
 
 print "sending packet on layer 2"
-sendp(packet, iface="eth1")
+sendp(packet, iface="eth0")
