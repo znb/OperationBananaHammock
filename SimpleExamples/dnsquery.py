4c248313d9666b68b0217fe9cd09bd112854b9c3
Matt <code@zonbi.org>
2013-09-17 21:33:36 +0200

    silly features

--- SimpleExamples/dnsquery.py
+++ SimpleExamples/dnsquery.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python
+# Silly DNS resolver which will default to Google if you let it
 
-# Silly DNS resolver
-
-from scapy.all import *
 import argparse
 import sys
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
 
-RESOLVER="8.8.8.8"
 
-
-def lookup(arg_domain):
-    """lookup a domain"""
-    a = sr1(IP(dst=RESOLVER)/UDP()/DNS(rd=1,qd=DNSQR(qname=arg_domain)),verbose=0)
-    return a[DNSRR].rdata
+def lookup(adomain, aresolver):
+    """lookup a domain"""    
+    a = sr1(IP(dst=aresolver)/UDP()/DNS(rd=1,qd=DNSQR(qname=adomain)),verbose=0)    
+    return a[DNSRR].rdata  
     
 
 def menu():
 
-    parser = argparse.ArgumentParser(description='basic DNS query', usage='%(prog)s -d domain')
+    parser = argparse.ArgumentParser(description='basic DNS query', usage='%(prog)s -d domain -r resolver')
+    parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.2')
     parser.add_argument('--domain', '-d', dest='domain', help='domain to lookup')
-    parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.1')
+    parser.add_argument('--resolver', '-r', dest='resolver', help='resolver to use', default='8.8.8.8')
     args = parser.parse_args()
-    arg_domain = args.domain
+    adomain = args.domain
+    aresolver = args.resolver
 
     if not args.domain:
         sys.exit(parser.print_help())
 
-    output = lookup(arg_domain)
-    print arg_domain + " >> " + output
+    output = lookup(adomain, aresolver)
+    print adomain + " >> " + output + "  (%s)" % aresolver
 
 
 if __name__ == '__main__':
     menu()
-
