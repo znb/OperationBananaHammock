6ec8bbaddb0b7ca8108f29bbbf83ed17c62f8f08
Matt <code@zonbi.org>
2013-09-17 20:30:18 +0200

    totes stole part of this code from bsb

---
+++ SimpleExamples/traceroute.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# simple traceroute script
+# ps. I totally stole this from bsb
+
+import sys 
+import argparse
+from scapy.all import *
+
+
+def traceroute(atarget):
+
+    print "begin trace... " + atarget
+    ans,unans=sr(IP(dst=atarget, ttl=(4,25),id=RandShort())/TCP(flags=0x2))
+    print ""
+    for snd,rcv in ans:
+        print snd.ttl, rcv.src, isinstance(rcv.payload, TCP)        
+
+
+def menu():
+
+    parser = argparse.ArgumentParser(description='simple traceroute in scapy', usage='%(prog)s -t target')
+    parser.add_argument('--target', '-t', dest='target', help='target to trace')
+    parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.1')
+    args = parser.parse_args()
+
+    if not args.target:
+        sys.exit(parser.print_help())
+
+    atarget = args.target
+    traceroute(atarget)
+
+
+if __name__ == '__main__':
+    menu()
