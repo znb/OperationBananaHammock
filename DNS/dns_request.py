b8521a9268ce4930aa5780d41fa51970b1321489
Matt <code@zonbi.org>
2013-09-17 21:38:49 +0200

    small changes for brucon

--- DNS/dns_request.py
+++ DNS/dns_request.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # Simple DNS request
 
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
-import sys
+
 
 # EDIT ME
 RESOLVER="8.8.8.8"
-HOST="blog.zonbi.org"
+HOST="2013.brucon.org"
 
 a = sr1(IP(dst=RESOLVER)/UDP()/DNS(rd=1,qd=DNSQR(qname=HOST)),verbose=0)
 print "Host: " + HOST + " >> " + a[DNSRR].rdata
