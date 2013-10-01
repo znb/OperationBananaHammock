8bbc146341ab73076f2038cc942f542672c50742
Matt <code@zonbi.org>
2013-10-01 18:56:54 +0200

    small changes from Brucon

--- Http/http_request.py
+++ Http/http_request.py
@@ -5,8 +5,8 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-HOSTNAME="2013.brucon.org"
-GETREQ="GET / HTTP/1.1\r\nHost: 2013.brucon.org\r\n\r\n"
+HOSTNAME="www.brucon.org"
+GETREQ="GET / HTTP/1.1\r\nHost: www.brucon.org\r\n\r\n"
 
 print "Syn"
 syn = IP(dst=HOSTNAME) / TCP(sport=666, dport=80, flags='S')
@@ -16,6 +16,6 @@ print "TCP Sequencing"
 request = IP(dst=HOSTNAME) / TCP(dport=80, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / GETREQ
 print "Reply"
 reply = sr1(request)
-print reply
+reply.show()
 
 print "We're done here"
