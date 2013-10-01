32354dad8a183bfa67641b8fd3fd732a773ce57e
Matt <code@zonbi.org>
2013-10-01 18:52:28 +0200

    small changes after Brucon feedback

--- Dhcp/dhcp-discover.py
+++ Dhcp/dhcp-discover.py
@@ -5,17 +5,15 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-
-print "Pre-game"
 conf.verb = 0
 conf.checkIPaddr = False
-hw="00:0c:29:be:e4:6d"
-print "Building packet"
 
+print "Building packet"
 e = Ether(dst="ff:ff:ff:ff:ff:ff")
 i = IP(src="0.0.0.0",dst="255.255.255.255")
 u = UDP(sport=68,dport=67)
-b = BOOTP(chaddr=hw,xid=8008135)
+# You will just need to modify the chaddr address below
+b = BOOTP(chaddr="00:0c:29:be:e4:6d",xid=31337)
 d = DHCP(options=[("message-type","discover"),"end"])
 
 dhcp_discover = e/i/u/b/d
@@ -24,5 +22,3 @@ print "Sending packet"
 reply = srp(dhcp_discover, iface="eth0", timeout=3)
 for a in reply:
 	print a.show()
-
-print "We're done here"
