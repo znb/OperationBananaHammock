c61f1f0ba734e8ba5453b3b5e1ebc321ef46b042
Matt <code@zonbi.org>
2013-10-01 18:45:49 +0200

    fixing the arp packet

--- Arp/arp-who-has.py
+++ Arp/arp-who-has.py
@@ -10,16 +10,12 @@ from scapy.all import *
 conf.verb = 0
 print "Building packet",
 e = Ether()
-e.type=int("0x806", 16)
+e.dst="ff:ff:ff:ff:ff:ff"
+e.type=0x806
 a = ARP()
-a.hwtype=int("0x1", 16)
-a.hwlen=int("6")
-a.plen=int("4")
 a.op="who-has"
 a.hwsrc="00:0c:29:be:e4:6d"
-a.hwdst="00:00:00:00:00:00" 
-a.psrc="10.0.0.71"
-a.pdst="10.0.0.253"
+a.pdst="172.16.206.2"
 packet = e/a
 
 print "\nSending packet on layer 2"
