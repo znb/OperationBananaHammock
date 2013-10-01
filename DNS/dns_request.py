04c97389dcd448014024af3cd891a0338d542066
Matt <code@zonbi.org>
2013-10-01 18:48:44 +0200

    small changes

--- DNS/dns_request.py
+++ DNS/dns_request.py
@@ -8,7 +8,7 @@ from scapy.all import *
 
 # EDIT ME
 RESOLVER="8.8.8.8"
-HOST="2013.brucon.org"
+HOST="www.brucon.org"
 
 a = sr1(IP(dst=RESOLVER)/UDP()/DNS(rd=1,qd=DNSQR(qname=HOST)),verbose=0)
 print "Host: " + HOST + " >> " + a[DNSRR].rdata
